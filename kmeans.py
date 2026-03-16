3597fb7863462d917679a60195739c367ff25aed
GIW065 <isaac.wegner@outlook.com>
2026-03-16 11:57:19 -0600

    File uploaded for implementation

---
+++ kmeans.py
@@ -0,0 +1,19 @@
+def euclidean_distance(p1, p2):
+    # Aysha to implement
+    pass
+
+def find_closest_centroid(point, centroids):
+    # Aysha to implement   
+    pass
+
+def update_centroids(data, labels, k):
+    # Priyanshu to implement
+    pass
+
+def has_converged(old, new, tol):
+    # Priyanshu to implement
+    pass
+
+def fit(data, k):
+    # Isaac to implement
+    pass
