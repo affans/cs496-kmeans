b08c03460b5ca0020680dfee34f83b4a4052b554
aysha.sparrowhawk1@yahoo.ca <ass740@uregina.ca>
2026-03-18 15:37:30 -0600

    Aysha uploaded her unit tests

---
+++ test_kmeans.py
@@ -0,0 +1,24 @@
+import pytest
+from aysha_task import euclidean_distance
+from aysha_task import find_closest_centroid
+
+def test_euc_empty():
+    with pytest.raises(ValueError):
+        euclidean_distance([], [])
+        
+def test_euc_string():
+    with pytest.raises(TypeError):
+        euclidean_distance(str, str)
+
+def test_euc_onestring():
+    with pytest.raises(TypeError):
+        euclidean_distance(str, [])
+
+
+def test_cen_empty():
+    with pytest.raises(ValueError):
+        find_closest_centroid([], [])
+        
+def test_cen_string():
+    with pytest.raises(TypeError):
+        find_closest_centroid(str, str)
